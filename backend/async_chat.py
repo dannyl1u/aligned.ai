4d5f742ad867d768757b4d672fa4cc811ecee865
dannyl1u <dannyjialiliu@gmail.com>
2024-09-14 13:20:02 -0400

    groq w/ llama

--- backend/async_chat.py
+++ backend/async_chat.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
 import asyncio
-from groq import AsyncGroq  # Import the AsyncGroq client
+from groq import AsyncGroq
 
 load_dotenv()
 
@@ -13,10 +13,10 @@ Ask me deep situation and interest-based questions to evaluate my personality so
 As I answer, use my previous answers to ask more meaningful and deep questions, provoking a thoughtful response from me.
 Ask questions that make me reveal what my values are.
 """
+
 messages = []
 
 async def chat():
-    # Initialize the GROQ async client with your API key
     client = AsyncGroq(api_key=GROQ_API_KEY)
 
     for i in range(10):
@@ -28,16 +28,13 @@ async def chat():
 
         print("Model response:")
 
-        # Call the GROQ API for generating a chat response asynchronously
         chat_completion = await client.chat.completions.create(
             messages=messages,
-            model="llama3-8b-8192"  # Replace with the appropriate model name
+            model="llama3-8b-8192"
         )
 
-        # Extract the response content
         print(chat_completion.choices[0].message.content)
 
-        # Append the assistant's response to the message list
         messages.append({'role': 'assistant', 'content': chat_completion.choices[0].message.content})
 
 asyncio.run(chat())
